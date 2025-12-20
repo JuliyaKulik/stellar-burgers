1088213668efb1a8cd167348f98e8d18fac2e0bf
JuliyaKulik <hey_julli@mail.ru>
2025-12-20 07:48:35 +0300

    исправлены замечания

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,10 +1,67 @@
 /// <reference types="cypress" />
 
+const SELECTORS = {
+  MODAL: '[data-cy="modal"]',
+  MODAL_CLOSE: '[data-cy="modal-close"]',
+  MODAL_OVERLAY: '[data-cy="modal-overlay"]',
+
+  INGREDIENT_DETAILS: '[data-cy="ingredient-details"]',
+  ORDER_NUMBER: '[data-cy="order-number"]',
+
+  BURGER_CONSTRUCTOR: '[data-cy="burger-constructor"]',
+
+  SELECT_BUNS_TEXT: 'Выберите булки',
+  SELECT_FILLINGS_TEXT: 'Выберите начинку',
+
+  API_INGREDIENTS: 'api/ingredients',
+  API_AUTH_USER: 'api/auth/user',
+  API_ORDERS: 'api/orders',
+
+  FIXTURE_INGREDIENTS: 'ingredients.json',
+  FIXTURE_USER: 'user.json',
+  FIXTURE_ORDER: 'order.json',
+
+  ACCESS_TOKEN_COOKIE: 'accessToken',
+  ACCESS_TOKEN_VALUE: 'test-access-token',
+  REFRESH_TOKEN_KEY: 'refreshToken',
+  REFRESH_TOKEN_VALUE: 'test-refresh-token',
+};
+
+const INGREDIENTS = {
+  FLUORESCENT_BUN: 'Флюоресцентная булка R2-D3',
+  KRASTOR_BUN: 'Краторная булка N-200i',
+  LUMINESCENT_FILLET: 'Филе Люминесцентного тетраодонтимформа',
+  SPICY_SAUCE: 'Соус Spicy-X',
+};
+
+const BUTTON_TEXTS = {
+  ADD: 'Добавить',
+  CREATE_ORDER: 'Оформить заказ',
+};
+
+const ORDER = {
+  NUMBER: '12345',
+};
+
+const getIngredientElement = (ingredientName: string) => {
+  return cy.contains(ingredientName).parents('li');
+};
+
+const addIngredient = (ingredientName: string) => {
+  getIngredientElement(ingredientName)
+    .find(`button:contains("${BUTTON_TEXTS.ADD}")`)
+    .click();
+};
+
+const checkModalClosed = () => {
+  cy.get(SELECTORS.MODAL).should('not.exist');
+  cy.url().should('eq', Cypress.config().baseUrl + '/');
+};
+
 describe('Конструктор бургеров - тесты по ТЗ', () => {
-  
   beforeEach(() => {
-    cy.intercept('GET', 'api/ingredients', { 
-      fixture: 'ingredients.json' 
+    cy.intercept('GET', SELECTORS.API_INGREDIENTS, { 
+      fixture: SELECTORS.FIXTURE_INGREDIENTS 
     }).as('getIngredients');
 
     cy.visit('/');
@@ -21,88 +78,67 @@ describe('Конструктор бургеров - тесты по ТЗ', () =>
 
   describe('Добавление ингредиента из списка в конструктор', () => {
     it('Добавляет булку по кнопке "Добавить"', () => {
-      cy.contains('Флюоресцентная булка R2-D3')
-        .parents('li')
-        .find('button:contains("Добавить")')
-        .click();
+      addIngredient(INGREDIENTS.FLUORESCENT_BUN);
       
-      cy.get('body').should('contain', 'Флюоресцентная булка R2-D3');
+      cy.get('body').should('contain', INGREDIENTS.FLUORESCENT_BUN);
     });
 
     it('Добавляет начинку по кнопке "Добавить"', () => {
-      cy.contains('Флюоресцентная булка R2-D3')
-        .parents('li')
-        .find('button:contains("Добавить")')
-        .click();
+      addIngredient(INGREDIENTS.FLUORESCENT_BUN);
+      addIngredient(INGREDIENTS.LUMINESCENT_FILLET);
       
-      cy.contains('Филе Люминесцентного тетраодонтимформа')
-        .parents('li')
-        .find('button:contains("Добавить")')
-        .click();
-      
-      cy.get('body').should('contain', 'Филе Люминесцентного тетраодонтимформа');
+      cy.get('body').should('contain', INGREDIENTS.LUMINESCENT_FILLET);
     });
 
     it('Добавляет соус по кнопке "Добавить"', () => {
-      cy.contains('Флюоресцентная булка R2-D3')
-        .parents('li')
-        .find('button:contains("Добавить")')
-        .click();
+      addIngredient(INGREDIENTS.FLUORESCENT_BUN);
+      addIngredient(INGREDIENTS.SPICY_SAUCE);
       
-      cy.contains('Соус Spicy-X')
-        .parents('li')
-        .find('button:contains("Добавить")')
-        .click();
-      
-      cy.get('body').should('contain', 'Соус Spicy-X');
+      cy.get('body').should('contain', INGREDIENTS.SPICY_SAUCE);
     });
   });
 
   describe('Работа модальных окон ингредиента', () => {
     it('Открывает модальное окно ингредиента при клике', () => {
-      cy.contains('Краторная булка N-200i').click();
+      cy.contains(INGREDIENTS.KRASTOR_BUN).click();
       
-      cy.get('[data-cy="modal"]').should('be.visible');
-      cy.get('[data-cy="ingredient-details"]').should('be.visible');
+      cy.get(SELECTORS.MODAL).should('be.visible');
+      cy.get(SELECTORS.INGREDIENT_DETAILS).should('be.visible');
 
-      cy.get('[data-cy="modal-close"]').click();
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.get(SELECTORS.MODAL_CLOSE).click();
+      checkModalClosed();
     });
 
     it('Закрывает модальное окно по клику на крестик', () => {
-      cy.contains('Краторная булка N-200i').click();
+      cy.contains(INGREDIENTS.KRASTOR_BUN).click();
       cy.wait(1000);
       
-      cy.get('[data-cy="modal-close"]').click();
-
-      cy.get('[data-cy="modal"]').should('not.exist');
-      cy.url().should('eq', Cypress.config().baseUrl + '/');
+      cy.get(SELECTORS.MODAL_CLOSE).click();
+      checkModalClosed();
     });
 
     it('Закрывает модальное окно по клику на оверлей', () => {
-      cy.contains('Краторная булка N-200i').click();
+      cy.contains(INGREDIENTS.KRASTOR_BUN).click();
       cy.wait(1000);
 
-      cy.get('[data-cy="modal-overlay"]').click({ force: true });
-
-      cy.get('[data-cy="modal"]').should('not.exist');
-      cy.url().should('eq', Cypress.config().baseUrl + '/');
+      cy.get(SELECTORS.MODAL_OVERLAY).click({ force: true });
+      checkModalClosed();
     });
   });
 
   describe('Создание заказа', () => {
     beforeEach(() => {
-      cy.intercept('GET', 'api/auth/user', { 
-        fixture: 'user.json' 
+      cy.intercept('GET', SELECTORS.API_AUTH_USER, { 
+        fixture: SELECTORS.FIXTURE_USER 
       }).as('getUser');
 
-      cy.intercept('POST', 'api/orders', { 
-        fixture: 'order.json' 
+      cy.intercept('POST', SELECTORS.API_ORDERS, { 
+        fixture: SELECTORS.FIXTURE_ORDER 
       }).as('createOrder');
 
-      cy.setCookie('accessToken', 'test-access-token');
+      cy.setCookie(SELECTORS.ACCESS_TOKEN_COOKIE, SELECTORS.ACCESS_TOKEN_VALUE);
       cy.window().then((win) => {
-        win.localStorage.setItem('refreshToken', 'test-refresh-token');
+        win.localStorage.setItem(SELECTORS.REFRESH_TOKEN_KEY, SELECTORS.REFRESH_TOKEN_VALUE);
       });
 
       cy.reload();
@@ -112,35 +148,27 @@ describe('Конструктор бургеров - тесты по ТЗ', () =>
     });
 
     it('Создает заказ с правильным номером', () => {
-      cy.contains('Флюоресцентная булка R2-D3')
-        .parents('li')
-        .find('button:contains("Добавить")')
-        .click();
-      
-      cy.contains('Соус Spicy-X')
-        .parents('li')
-        .find('button:contains("Добавить")')
-        .click();
+      addIngredient(INGREDIENTS.FLUORESCENT_BUN);
+      addIngredient(INGREDIENTS.SPICY_SAUCE);
 
-      cy.get('body').should('contain', 'Флюоресцентная булка R2-D3');
-      cy.get('body').should('contain', 'Соус Spicy-X');
+      cy.get('body').should('contain', INGREDIENTS.FLUORESCENT_BUN);
+      cy.get('body').should('contain', INGREDIENTS.SPICY_SAUCE);
 
-      cy.contains('Оформить заказ').click();
+      cy.contains(BUTTON_TEXTS.CREATE_ORDER).click();
 
       cy.wait('@createOrder');
 
-      cy.get('[data-cy="modal"]').should('be.visible');
-      cy.get('[data-cy="order-number"]').should('contain', '12345');
+      cy.get(SELECTORS.MODAL).should('be.visible');
+      cy.get(SELECTORS.ORDER_NUMBER).should('contain', ORDER.NUMBER);
 
-      cy.get('[data-cy="modal-close"]').click();
+      cy.get(SELECTORS.MODAL_CLOSE).click();
 
-      cy.get('[data-cy="modal"]').should('not.exist');
+      cy.get(SELECTORS.MODAL).should('not.exist');
 
-      cy.get('[data-cy="burger-constructor"]').within(() => {
-        cy.contains('Выберите булки').should('be.visible');
-        cy.contains('Выберите начинку').should('be.visible');
+      cy.get(SELECTORS.BURGER_CONSTRUCTOR).within(() => {
+        cy.contains(SELECTORS.SELECT_BUNS_TEXT).should('be.visible');
+        cy.contains(SELECTORS.SELECT_FILLINGS_TEXT).should('be.visible');
       });
     });
   });
 });
-
